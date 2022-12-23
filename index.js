94a6f05a6cb33f98a3e7b3174b25defc63c5f4e3
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-23 22:37:46 +0600

    re connected to db

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfjpd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://Doctors-portal:Q6VIKTngLAi5EqZg@cluster0.jfjpd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
